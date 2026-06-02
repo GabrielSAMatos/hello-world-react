421d1c07be77c910c2ce90af6fc6abb326896b4f
Gabriel Matos <matos7771@hotmail.com>
2026-06-02 11:14:34 -0300

    first hello world react :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  function hello(){
+    return "Hello, world!"
+  }
+
+  const url = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Ffree-photo%2Fvividly-colored-hummingbird-nature_23-2151495292.jpg%3Fsize%3D626%26ext%3Djpg&f=1&nofb=1&ipt=8f641d2c0ccba054cc439a5af68b65a095ec1eaf9ed95e93384254952a1035eb'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{hello()}</h1>
+      <img src={url} alt="150x150"/>
     </div>
   );
 }
